a34cb682f9f63b09cb66d2d6bf6a33bdf4e15729
ippei-shimizu <ippei.shimizu.32@gmail.com>
2023-05-27 12:25:43 +0900

    4-5-4-1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_27_030244) do
+ActiveRecord::Schema.define(version: 2023_05_27_032043) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", limit: 30, null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2023_05_27_030244) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
