9d173772d9479598fbe1f0ff2381a08ac73c8155
ippei-shimizu <ippei.shimizu.32@gmail.com>
2023-05-28 17:14:02 +0900

    chapter5完了

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -19,10 +19,10 @@ describe "タスク管理機能", type: :system do
     end
 
     describe "一覧表示機能" do
-    context "ユーザーAがログインしているとき" do
-      let(:login_user) { user_a }
-      it_behaves_like "ユーザーAが作成したタスクが表示される"
-    end
+      context "ユーザーAがログインしているとき" do
+        let(:login_user) { user_a }
+        it_behaves_like "ユーザーAが作成したタスクが表示される"
+      end
 
     context "ユーザーBがログインしているとき" do
       let(:login_user) { user_b }
@@ -45,4 +45,33 @@ describe "タスク管理機能", type: :system do
     end
   end
 
+    describe "新規作成機能" do
+      let(:login_user) { user_a }
+      let(:task_name) { "新規作成のテストを書く" }
+
+      before do
+        visit new_task_path
+        fill_in "名称", with: task_name
+        click_button "登録する"
+      end
+
+      context "新規作成画面で名称を入力したとき" do
+        let(:task_name) { "新規作成のテストを書く" }
+
+        it "正常に登録させる" do
+          expect(page).to have_selector ".alert-success", text: "新規作成のテストを書く"
+        end
+      end
+
+      context "新規作成画面で名称を入力しなかったとき" do
+        let(:task_name) {""}
+
+        it "エラーとなる" do
+          within "#error_explanation" do
+            expect(page).to have_content "名称 を入力してください"
+        end
+      end
+    end
+  end
 end
+
